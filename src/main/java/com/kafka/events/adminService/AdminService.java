582e5bc39ba336aa85826d526869da712ae394b9
yihune21 <zewdieyihune@gmail.com>
2026-08-05 12:07:41 +0300

    initial commit. create admin service that create admin client , topic and shut down the admin client.

---
+++ src/main/java/com/kafka/events/adminService/AdminService.java
@@ -0,0 +1,40 @@
+package com.kafka.events.adminService;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class AdminService {
+
+        private final AdminClient adminClient;
+
+        public AdminService(){
+            this.adminClient = createAdmin();
+        }
+
+        private AdminClient createAdmin(){
+            Properties props = new Properties();
+            props.setProperty(
+                    "bootstrap.servers",
+                    "localhost:9092"
+            );
+            return AdminClient.create(props);
+        }
+
+        public void createTopic(String topicName){
+            NewTopic topic = new NewTopic(
+                    topicName,
+                    3,
+                    (short) 1
+            );
+
+            adminClient.createTopics(Collections.singleton(topic));
+        }
+
+        public void shutdown(){
+            adminClient.close();
+        }
+
+}
---
+++ src/main/java/com/kafka/events/producerService/ProducerService.java
@@ -0,0 +1,10 @@
+package com.kafka.events.producerService;
+
+
+public class ProducerService {
+
+
+    //
+
+
+}
